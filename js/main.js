3f222fe3ea9a070c009d51587ef3f55a92a8a363
aharaldson <a.haraldson@gmail.com>
2023-11-14 14:20:08 -0700

    formatting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+/* pseudocode //
+//
+// A BET MUST BE PLACED ON PASS LINE TO START
+// const sections = document.getElementById('board').addEventListener('click', function(evt) {...
+// [not sure how to handle the betting aspect, definitely an icebox item]
+//
+// SHOOTER ROLLS 2 DICE (NO POINT SET)
+// firstRoll: function() {
+// const randNum1 = Math.floor(Math.random() * 6) + 1 for each die
+//
+// FIRST ROLL (COMEOUT) 7 & 11 WINS, 2, 3, 12 LOSES
+// passLine: {
+//  numsWin: [7, 11]
+//  numsLose: [2, 3, 12]
+// }
+//
+// IF ANY OTHER NUM IS ROLLED (4, 5, 6, 8, 9, 10), SET POINT THERE
+// passLine: {
+//  setPoint: []
+// }
+//
+// ONCE POINT IS SET, BETS CAN BE PLACED ANYWHERE AND SHOOTER ROLLS UNTIL WIN (POINT NUM) OR LOSE (7)
+//
+// IF POINT IS ROLLED BEFORE 7, PASS LINE BETS WIN
+// IF 7 ROLLED BEFORE POINT, DON'T PASS BETS WIN, SHOOTER'S TURN IS OVER
+
 // pseudocode //
 //
 // A BET MUST BE PLACED ON PASS LINE TO START
@@ -8,99 +34,118 @@
 // IF POINT IS ROLLED BEFORE 7, PASS LINE BETS WIN
 // IF 7 ROLLED BEFORE POINT, DON'T PASS BETS WIN, SHOOTER'S TURN IS OVER
 //
-//
-// VARIABLES //
- let rollTotal = null;
- let currentPoint = null;
- let shooterData = {
-    point = null,
-    bet = null,
-    gameState = null,
-    bank = 100,
- }
+*/
+/*---- VARIABLES ----*/
+let rollTotal = null;
+let shooterData = {
+   point: null,
+   bet: 0,
+   gameState: null,
+   bank: 100,
+};
 
- let newGame = fuction () {
-    shooterData.point = 0;
-    shooterData.bet = 0;
-    shooterData.gameState = 'come'
- }
+/*---- ELEMENTS ----*/
+const rollButtonEl = document.getElementById('rollbutton')
+const betAddFiveEl = document.getElementById('betaddfive')
+const currentBetEl = document.getElementById('currentbet')
+const comeOutNumEl = document.getElementById('comeoutnumber')
+const rollEl = document.getElementById('roll')
+const winLoseEl = document.getElementById('winlose')
+const playfieldEl = document.getElementById('playfield')
 
- let winner = function() {
-    console.log('winner!');
-    let winnings = (shooterData.bet * 2);
-    shooterData.bank = shooterData.bank + winnings;
-    newGame();
+/*---- EVENT LISTENERS ----*/
+betAddFiveEl.addEventListener('click', betAddFive)
+rollButtonEl.addEventListener('click', rollDice)
+
+
+function newGame()) {
+   console.log('new game');
+   shooterData.point = 0;
+   shooterData.bet = 0;
+   shooterData.gameState = 'come'
+   currentBetEl.innerText = "$" + shooterData.bet;
+};
+
+function winner() {
+   console.log('winner!');
+   let winnings = (shooterData.bet * 2);
+   shooterData.bank = shooterData.bank + winnings;
+   comeOutNumEl.innerText = shooterData.point;
+   winLoseEl.innerHTML = '<h3>You won $' + winnings + '! <br>Place another bet to play again</h3>';
+   newGame();
+   return true;
+};
+
+function loser() {
+   console.log('loser!');
+   comeOutNumEl.innerText = shooterData.point;
+   winLoseEl.innerHTML = '<h3>You lost!<br>Place another bet to play again</h3>';
+   newGame();
+   return true;
+};
+
+function newPoint() {
+    shooterData.gameState = 'point';
+    comeOutNumEl.innerText = shooterData.point;
     return true;
- }
+};
 
- let loser = function() {
-    console.log('loser!');
-    newGame();
-    return true;
- }
+function betAddFive() {
+   let betValue = event.target.value;
+   betValue = Number(betValue);
+   shooterData.bet = shooterData.bet + betValue;
+   shooterData.bank = shooterData.bank - betValue;
+   currentBetEl.innerText = "$" + shooterData.bet;
+   console.log('add $5');
+   return true;
+};
 
-let newPoint
+function rollDice() {
+    const dice1 = Math.floor((Math.random() * 6) + 1);
+    const dice2 = Math.floor((Math.random() * 6) + 1);
+    rollTotal = (dice1 + dice2);
+    rollEl.innerText = rollTotal;
+    console.log('dice rolled');
+   playCraps();
+};
 
 // GAME STATES (functions?) // 
 // i feel quite good about this part, now I need to make it work with everything else
 
-const playCraps = function () {
-    if (shooterData.point < 1) {
-        if (rollTotal === 7 || rollTotal === 11) {
-            shooterData.point = rollTotal;
-            winner();
-            return true;
-        } else if (rollTotal === 2 || rollTotal === 3 || rollTotal === 12) {
-            shooterData.point = rollTotal;
-            loser();
-            return true;
-        } else {
-            shooterData.point = rollTotal;
-            newPoint();
-            return true;
-        }
-    }
-    if (shooterData.point > 0) {
-        if (rollTotal === 7) {
-            loser();
-            return true;
-        } else if (shooterData.point === rollTotal) {
-            shooterData.point = rollTotal;
-            winner();
-            return true;
-        } else {}
-    }
-    return true;
+function playCraps() {
+   if (shooterData.point < 1) {
+       if (rollTotal === 7 || rollTotal === 11) {
+           shooterData.point = rollTotal;
+           winner();
+           console.log("win on comeout");
+           return true;
+       } else if (rollTotal === 2 || rollTotal === 3 || rollTotal === 12) {
+           shooterData.point = rollTotal;
+           loser();
+         console.log("craps");
+           return true;
+       } else {
+           shooterData.point = rollTotal;
+           newPoint();
+           console.log("new point");
+           return true;
+       }
+   }
+   if (shooterData.point > 0) {
+       if (rollTotal === 7) {
+           loser();
+           console.log("seven out");
+           return true;
+       } else if (shooterData.point === rollTotal) {
+           shooterData.point = rollTotal;
+           winner();
+           console.log("point hit");
+           return true;
+       } else {}
+   }
+   return true;
 
-}
-// 
-// GAMEBOARD // (sections, eventlisteners)
-const sections = document.getElementById('playfield').addEventListener('click', function(evt) {
-     console.log(evt.target.id)
-// [not sure how to handle the betting aspect, definitely an icebox item]
+};
 
-// RULES //
- const rules = {
-   firstRoll: {
-        numsToWin: [7, 11],
-        numsToLose: [2, 3, 12],
-        numsPass: [4, 5, 6, 8, 9, 10],
-        setPoint: {
-     
-
- 
- const rollDice = function() {
-     const dice1 = Math.floor((Math.random() * 6) + 1);
-     const dice2 = Math.floor((Math.random() * 6) + 1);
-     rollTotal = (dice1 + dice2);
-
- 
- passLine: {
-  numsWin: [7, 11]
-  numsLose: [2, 3, 12]
- }
-
- passLine: {
-  setPoint: []
- }
-}
+newGame();
+playCraps();
